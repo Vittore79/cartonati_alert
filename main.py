d9327e020900c64a053a98d083392004b3b58c2f
Vittore Maltese <rastotv2000@gmail.com>
2026-05-21 16:08:33 +0200

    first bot

---
+++ main.py
@@ -0,0 +1,107 @@
+import requests
+import schedule
+import time
+import asyncio
+from telegram import Bot
+
+# =========================
+# CONFIG
+# =========================
+
+TELEGRAM_TOKEN = "8312714597:AAGXOyaW8b1k_PBS0OYf92MdgoDP2fImJXs"
+CHAT_ID = "7494998558"
+NEWS_API_KEY = "66c39f4197af4b1eb102c8308528daa1"
+
+KEYWORDS = [
+    "Inter",
+    "FIGC",
+    "ultras inter",
+    "procura inter",
+    "indagini inter",
+    "scandalo",
+    "scandalo marotta",
+    "corona inter",
+    "calcioscommesse"
+]
+
+bot = Bot(token=TELEGRAM_TOKEN)
+
+already_sent = set()
+
+# =========================
+# INVIO TELEGRAM
+# =========================
+
+import asyncio
+
+def send_telegram_message(message):
+    asyncio.run(
+        bot.send_message(chat_id=CHAT_ID, text=message)
+    )
+
+# =========================
+# CERCA NEWS
+# =========================
+
+def search_news():
+
+    print("Controllo news...")
+
+    for keyword in KEYWORDS:
+
+        url = (
+            f"https://newsapi.org/v2/everything?"
+            f"q={keyword}&"
+            f"language=it&"
+            f"sortBy=publishedAt&"
+            f"apiKey={NEWS_API_KEY}"
+        )
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print("Errore API:", response.text)
+            continue
+
+        data = response.json()
+
+        articles = data.get("articles", [])
+
+        for article in articles[:5]:
+
+            title = article["title"]
+            link = article["url"]
+            source = article["source"]["name"]
+
+            unique_id = title + link
+
+            if unique_id in already_sent:
+                continue
+
+            already_sent.add(unique_id)
+
+            message = (
+                f"🚨 NUOVA NEWS\n\n"
+                f"🔎 Keyword: {keyword}\n\n"
+                f"📰 {title}\n\n"
+                f"📌 Fonte: {source}\n\n"
+                f"🔗 {link}"
+            )
+
+            send_telegram_message(message)
+
+            print("Inviata:", title)
+
+# =========================
+# SCHEDULER
+# =========================
+
+schedule.every(5).minutes.do(search_news)
+
+print("BOT AVVIATO")
+
+search_news()
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
